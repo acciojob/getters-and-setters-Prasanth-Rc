b495e66511e6200d0ab9e9fb0391be6536252867
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-13 09:42:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 //complete this code
-class Person {}
+class Person {
+	private _name: string;
+    private _age: number;
 
-class Student extends Person {}
+    constructor(name: string, age: number) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Teacher extends Person {}
+	get name():string{
+		return this._name;
+	}
+
+	set age(age:number){
+		this._age=age;
+	}
+
+	get age(): number {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+	constructor(name: string, age: number) {
+        super(name, age);
+    }
+
+	study(): void {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+	constructor(name: string, age: number) {
+        super(name, age);
+    }
+
+	teach(): void {
+        console.log(`${this.name} is teaching`);
+    }
+}
+
+// Creating a Person instance
+const person = new Person("John", 25);
+console.log(person.name);  // Output: John
+
+person.age = 30;  // Using the setter to change the age
+console.log(person.age);  // Output: 30
+
+// Creating a Student instance
+const student = new Student("john", 30);
+student.study();  // Output: Alice is studying
+
+// Creating a Teacher instance
+const teacher = new Teacher("john", 30);
+teacher.teach();  // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
