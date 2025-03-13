513d09f1332c1eeec5dba691367694387682c7f6
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-13 18:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //complete this code
 class Person {
-	private _name: string;
+    private _name: string;
     private _age: number;
 
     constructor(name: string, age: number) {
@@ -8,35 +8,35 @@ class Person {
         this._age = age;
     }
 
-	get name():string{
-		return this._name;
-	}
+    get name(): string {
+        return this._name;
+    }
 
-	set age(age:number){
-		this._age=age;
-	}
+    set age(age: number) {
+        this._age = age;
+    }
 
-	get age(): number {
+    get age(): number {
         return this._age;
     }
 }
 
 class Student extends Person {
-	constructor(name: string, age: number) {
+    constructor(name: string, age: number) {
         super(name, age);
     }
 
-	study(): void {
+    study(): void {
         console.log(`${this.name} is studying`);
     }
 }
 
 class Teacher extends Person {
-	constructor(name: string, age: number) {
+    constructor(name: string, age: number) {
         super(name, age);
     }
 
-	teach(): void {
+    teach(): void {
         console.log(`${this.name} is teaching`);
     }
 }
