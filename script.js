57f6bf130f29e6153dd8344bed068ce1f5c895e0
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-24 09:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
 //complete this code
 class Person {
-    private _name: string;
-    private _age: number;
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-    constructor(name: string, age: number) {
-        this._name = name;
-        this._age = age;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get name(): string {
-        return this._name;
-    }
+  set age(age) {
+    this._age = age;
+  }
 
-    set age(age: number) {
-        this._age = age;
-    }
-
-    get age(): number {
-        return this._age;
-    }
+  // Getter for age
+  get age() {
+    return this._age;
+  }
 }
 
 class Student extends Person {
-    constructor(name: string, age: number) {
-        super(name, age);
-    }
+  constructor(name, age) {
+    super(name, age);
+  }
 
-    study(): void {
-        console.log(`${this.name} is studying`);
-    }
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-    constructor(name: string, age: number) {
-        super(name, age);
-    }
+  constructor(name, age) {
+    super(name, age);
+  }
 
-    teach(): void {
-        console.log(`${this.name} is teaching`);
-    }
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
